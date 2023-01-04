bae78ece5d0dfcd49b4542a49ab8c18eaf00e952
raquel531 <raqueloureirorodrigues@gmail.com>
2023-01-04 18:53:16 +0000

    actualização homepage e video-channel

---
+++ src/app/components/tag/tag.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.scss']
+})
+export class TagComponent implements OnInit {
+
+  @Input() tags= '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
