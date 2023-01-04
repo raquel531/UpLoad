c372c42ff2e4b32f7cfa683eb063b7ef20e428b2
raquel531 <raqueloureirorodrigues@gmail.com>
2023-01-04 19:58:46 +0000

    componente video

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -1,6 +1,5 @@
 import {Injectable, Pipe, PipeTransform} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import { Pipe, PipeTransform } from '@angular/core';
 
 import { DomSanitizer } from "@angular/platform-browser";
 
